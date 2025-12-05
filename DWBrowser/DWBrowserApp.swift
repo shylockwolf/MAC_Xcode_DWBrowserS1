6ce5a7a25d4a2132fada1597129759583038674c
shylockwolf <shylockwolf@yahoo.com>
2025-12-05 10:31:45 +0800

    5th --- Stable Version 1.0

--- DWBrowser/DWBrowserApp.swift
+++ DWBrowser/DWBrowserApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct DWBrowserApp: App {
@@ -14,5 +15,21 @@ struct DWBrowserApp: App {
             ContentView()
                 .frame(minWidth: 800, minHeight: 600)
         }
+        .commands {
+            CommandGroup(replacing: .help) {
+                Button("About DWBrowser") {
+                    showAboutDialog()
+                }
+                .keyboardShortcut("/")
+            }
+        }
+    }
+    
+    private func showAboutDialog() {
+        let alert = NSAlert()
+        alert.messageText = "DWBrowser"
+        alert.informativeText = "Version v1.0\n\nAuthor: Shylock Wolf\nCreation Date: 2025-11-17"
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
     }
 }
